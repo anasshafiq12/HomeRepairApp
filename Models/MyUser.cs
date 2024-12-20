c54d225dff5ff7c9710e3445b501f2190a2b60dd
anasshafiq12 <139551728+anasshafiq12@users.noreply.github.com>
2024-12-20 15:17:14 +0500

    3rd

---
+++ Models/MyUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace HouseRepairApp.Models
+{
+    public class MyUser:IdentityUser
+    {
+        public string Name {  get; set; }
+        public string Phone {  get; set; }
+        public string Address { get; set; }
+    }
+}
