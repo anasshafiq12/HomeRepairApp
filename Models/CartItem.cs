17ea1c35f1db078b19728351182105cf4c496d7e
anasshafiq12 <anasshafiq9988@gmail.com>
2024-12-21 11:10:50 +0500

    changes

--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -4,7 +4,9 @@ namespace HouseRepairApp.Models
 {
     public class CartItem
     {
-        
+        [Key]
+        public int ItemId { get; set; }
+        public string ImageUrl { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public float Price { get; set; }
