285ed7b52d773b6cf5017a20237a6fde6a7f9750
anasshafiq12 <anasshafiq9988@gmail.com>
2024-12-21 14:03:43 +0500

    bookingDone

--- obj/Debug/net8.0/HouseRepairApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/HouseRepairApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HouseRepairApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+026a5ae43956e402c1962b79faf62080353d4249")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c50ce0ad86ae158983f16d84d2dd22058e19887")]
 [assembly: System.Reflection.AssemblyProductAttribute("HouseRepairApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HouseRepairApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
