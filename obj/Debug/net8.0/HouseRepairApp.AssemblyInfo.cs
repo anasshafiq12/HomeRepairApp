a9b385fcaf956c952c13ad35deda6b3f721df645
anasshafiq12 <anasshafiq9988@gmail.com>
2024-12-25 13:05:00 +0500

    working orders

--- obj/Debug/net8.0/HouseRepairApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/HouseRepairApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HouseRepairApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37c9cac530e1eef2d83b560f695b030b47c68bf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81615592be5ea114bb13d51d729e71ab6dd0e14e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HouseRepairApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HouseRepairApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
