739b0ae50bef0069261baf6b8f46c43ee120e2ee
AnasShafiq07 <bsef22m041@pucit.edu.pk>
2025-01-06 21:18:23 +0500

    view details

--- obj/Debug/net8.0/HouseRepairApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/HouseRepairApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HouseRepairApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f00061ece8e73939caad441d9601852b2741a8da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dca0e592117260d403c83dcb0d0964b6c441dd59")]
 [assembly: System.Reflection.AssemblyProductAttribute("HouseRepairApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HouseRepairApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
